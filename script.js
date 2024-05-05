f058c16396994fc029f66bae5a8c34938b0dc306
Rituraj Sinha Mahapatra <67279998+riturajmahapatra@users.noreply.github.com>
2024-05-05 16:18:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// data from script2.js
+var data = require('./script2');
+
+console.log(data);
+
+// figlet
+var figlet = require('figlet');
+
+figlet('AI Thinkers', function (err, figletString) {
+  if (err) {
+    console.log('something went wrong');
+    return console.log(err);
+  }
+
+  console.log(figletString);
+});
+
+// express connection
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello');
+});
+
+app.listen(4000, function () {
+  console.log('server is running');
+});
---
+++ script2.js
@@ -0,0 +1,4 @@
+var a = 12;
+var b = 12354;
+
+module.exports = { first: a, second: b };
